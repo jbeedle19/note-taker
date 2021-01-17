641676c3bb9066e95c6ceb1e02dbe578e55bfedb
jbeedle19 <josh.beedle@gmail.com>
2021-01-17 12:18:05 -0500

    html routes created and working

--- routes/htmlRoutes/index.js
+++ routes/htmlRoutes/index.js
@@ -1,3 +1,17 @@
 // Routes to html pages below
 // GET /notes should return notes.html
 // GET * should return index.html
+const path = require('path');
+const router = require('express').Router();
+
+// Route for index.html
+router.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, '../../public/index.html'));
+});
+
+// Route for notes.html
+router.get('/notes', (req, res) => {
+    res.sendFile(path.join(__dirname, '../../public/notes.html'));
+});
+
+module.exports = router;
