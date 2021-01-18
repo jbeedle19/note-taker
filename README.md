13479bcca111b82a3986a0211c2919f615dc31ff
jbeedle19 <josh.beedle@gmail.com>
2021-01-17 19:22:43 -0500

    updated README

--- README.md
+++ README.md
@@ -1,9 +1,12 @@
 # Note Taker
 
 ## Description
-
+A website for users to create and store notes in order to better organize their lives.
+## Website
+https://note-taker-019.herokuapp.com/
 ## Screenshots
-
+![screenshot](./assets/images/screenshot-1.png)
+![screenshot](./assets/images/screenshot-2.png)
 ## Built With
 * HTML, CSS, JavaScript
 * Bootstrap
--- routes/apiRoutes/index.js
+++ routes/apiRoutes/index.js
@@ -41,15 +41,15 @@ router.post('/notes', (req, res) => {
     }
 });
 
-// DELETE route for deleting a note
-router.delete('/notes/:id', (req, res) => {
-    console.log('you are trying to delete');
-});
-
 // BONUS
 // DELETE route for deleting a note
 // DELETE /api/notes/:id should receive a query parameter containing the id of a note to delete. 
 // In order to delete a note, you'll need to read all notes from the db.json file, remove the note 
 // with the given id property, and then rewrite the notes to the db.json file.
+//STOPPED HERE, WORKING ON DELETING NOTES
+// DELETE route for deleting a note
+router.delete('/notes/:id', (req, res) => {
+    console.log('you are trying to delete a note');
+});
 
 module.exports = router;
